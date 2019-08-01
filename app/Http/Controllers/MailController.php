ff00ee5371430217d73f7c1658cb171c71d0706f
rizwan-hiroli <43402736+rizwan-hiroli@users.noreply.github.com>
2019-08-01 15:07:29 +0530

    mail controller updated

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -10,6 +10,7 @@ use App\User;
 class MailController extends Controller
 {
    public function SendMail(){
+    //Mail controller updated. 
    	$users  = User::get()->toArray();
    	SendEmail::dispatch($users)->delay(now()->addMinutes(1));
    	return back();
